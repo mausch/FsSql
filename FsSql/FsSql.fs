633c791abb1029578aaded4fa9bf2a9ee568b964
mausch <mauricioscheffer@gmail.com>
2010-08-11 19:20:16 -0300

    removed command wrapper, not using it

--- FsSql/FsSql.fs
+++ FsSql/FsSql.fs
@@ -7,32 +7,6 @@ open System.Linq
 open System.Text.RegularExpressions
 open Microsoft.FSharp.Reflection
 
-type DbCommandWrapper(cmd: IDbCommand) =
-    let doAndDispose f =
-        try
-            f()
-        finally
-            cmd.Connection.Dispose()
-            cmd.Dispose()
-            
-    interface IDbCommand with
-        member x.Cancel() = cmd.Cancel()
-        member x.CreateParameter() = cmd.CreateParameter()
-        member x.Dispose() = cmd.Dispose()
-        member x.Prepare() = cmd.Prepare()
-        member x.CommandText with get() = cmd.CommandText and set v = cmd.CommandText <- v
-        member x.CommandTimeout with get() = cmd.CommandTimeout and set v = cmd.CommandTimeout <- v
-        member x.CommandType with get() = cmd.CommandType and set v = cmd.CommandType <- v
-        member x.Connection with get() = cmd.Connection and set v = cmd.Connection <- v
-        member x.Parameters with get() = cmd.Parameters
-        member x.Transaction with get() = cmd.Transaction and set v = raise <| NotSupportedException()
-        member x.UpdatedRowSource with get() = cmd.UpdatedRowSource and set v = cmd.UpdatedRowSource <- v
-        member x.ExecuteNonQuery() = doAndDispose cmd.ExecuteNonQuery
-        member x.ExecuteReader() = cmd.ExecuteReader()
-        member x.ExecuteReader b = cmd.ExecuteReader b
-        member x.ExecuteScalar() = doAndDispose cmd.ExecuteScalar
-            
-
 
 let PrintfFormatProc (worker: string * obj list -> 'd)  (query: PrintfFormat<'a, _, _, 'd>) : 'a =
     if not (FSharpType.IsFunction typeof<'a>) then
