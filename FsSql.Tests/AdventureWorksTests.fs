3e9841b4db56a7dc23e8c2482464ac4b90b8ac7a
mausch <mauricioscheffer@gmail.com>
2010-08-26 10:48:33 -0300

    adventureworks in SQL Server

---
+++ FsSql.Tests/AdventureWorksTests.fs
@@ -0,0 +1,19 @@
+﻿module AdventureWorksTests
+
+open System
+open System.Data
+open System.Data.SqlClient
+open MbUnit.Framework
+
+let createConnection() =
+    let c = new SqlConnection("Data Source=.\\sqlexpress;initial catalog=AdventureWorks;Integrated Security=SSPI;")
+    c.Open()
+    c :> IDbConnection
+
+let connMgr = Sql.withNewConnection createConnection
+
+[<Test>]
+let select() =
+    let l = Sql.execReaderF connMgr "select * from HumanResources.Department" |> List.ofDataReader
+    printfn "%d" l.Length
+    ()
