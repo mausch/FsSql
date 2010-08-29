eef8f2f88439113b830b0f3ed26c7c3c8c04e241
mausch <mauricioscheffer@gmail.com>
2010-08-29 18:27:52 -0300

    documentation

--- FsSql/FSharpTypeExtensions.fs
+++ FsSql/FSharpTypeExtensions.fs
@@ -7,15 +7,18 @@ open Microsoft.FSharp.Core
 
 [<CompilationRepresentationAttribute(CompilationRepresentationFlags.ModuleSuffix)>]
 module FSharpType =
+    /// Returns true if type is an Option type
     let IsOption (t: Type) = 
         if t.IsGenericType
             then t.GetGenericTypeDefinition() = typedefof<option<_>>
             else false
 
+    /// Returns true if type is a list
     let IsList (t: Type) =
         if t.IsGenericType
             then t.GetGenericTypeDefinition() = typedefof<list<_>>
             else false
         
+    /// Creates an option type
     let MakeOptionType (t: Type) =
         typedefof<option<_>>.MakeGenericType [| t |]
--- FsSql/FSharpValueExtensions.fs
+++ FsSql/FSharpValueExtensions.fs
@@ -6,14 +6,25 @@ open Microsoft.FSharp.Reflection
 
 [<CompilationRepresentationAttribute(CompilationRepresentationFlags.ModuleSuffix)>]
 module FSharpValue =
+    /// <summary>
+    /// Creates a None option for type <paramref name="t"/>
+    /// </summary>
     let MakeOptionNone (t: Type) = 
         let opt = FSharpType.MakeOptionType t
         opt.InvokeMember("None", BindingFlags.Public ||| BindingFlags.Static ||| BindingFlags.GetProperty, null, null, null)
 
+    /// <summary>
+    /// Creates a Some option for type <paramref name="t"/> and value <paramref name="value"/>
+    /// </summary>
     let MakeOptionSome (t: Type) (value: obj) =
         let opt = FSharpType.MakeOptionType t
         opt.InvokeMember("Some", BindingFlags.Public ||| BindingFlags.Static ||| BindingFlags.InvokeMethod, null, null, [| value |])
-        
+
+    /// <summary>
+    /// Gets the value associated with an option
+    /// </summary>
+    /// <exception cref="System.NullReferenceException"><paramref value="opt"/> is null</exception>
+    /// <exception cref="System.ArgumentException"><paramref value="opt"/> is not an option</exception>
     let GetOptionValue (opt: obj) =
         if opt = null 
             then nullArg "opt"
@@ -22,29 +33,46 @@ module FSharpValue =
             then invalidArg "opt" "Object must be of option type"
         unbox <| t.InvokeMember("Value", BindingFlags.Public ||| BindingFlags.Instance ||| BindingFlags.GetProperty, null, opt, null)
 
+    /// Returns true if object is of option type
     let IsOption (opt: obj) =
         if opt = null
             then false
             else FSharpType.IsOption (opt.GetType())
 
+    /// <summary>
+    /// Returns true if object is None
+    /// </summary>
+    /// <exception cref="System.ArgumentException">Argument is not an option</exception>
     let IsNone (opt: obj): bool =
         if not (IsOption opt)
             then invalidArg "opt" "Object must be of option type"
             else unbox <| opt.GetType().GetMethod("get_IsNone").Invoke(null, [| opt |])
 
+    /// <summary>
+    /// Returns true if object is Some x
+    /// </summary>
+    /// <exception cref="System.ArgumentException">Argument is not an option</exception>
     let IsSome (opt: obj): bool =
         if not (IsOption opt)
             then invalidArg "opt" "Object must be of option type"
             else unbox <| opt.GetType().GetMethod("get_IsSome").Invoke(null, [| opt |])
 
+    /// <summary>
+    /// OptionType if value to match is an Option, otherwise NotOptionType
+    /// </summary>
+    /// <exception cref="System.ArgumentException">Argument is not an option</exception>
     let (|OptionType|NotOptionType|) x = 
         if IsOption x
             then OptionType
             else NotOptionType
 
+    /// <summary>
+    /// Extracts value associated with an Option.
+    /// </summary>
+    /// <exception cref="System.ArgumentException">Argument is not an option</exception>
     let (|OSome|_|) (x: obj) =
         if not (IsOption x)
-            then invalidArg "a" "Object must be of option type"
+            then invalidArg "x" "Object must be of option type"
             else 
                 if IsNone x
                     then None
