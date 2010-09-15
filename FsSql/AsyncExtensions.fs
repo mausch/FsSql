c18e6389a11f7b797a6ca7821c9a534c33ee3bd8
mausch <mauricioscheffer@gmail.com>
2010-09-15 18:21:07 -0300

    removed silly redundancies

--- FsSql/AsyncExtensions.fs
+++ FsSql/AsyncExtensions.fs
@@ -1,29 +1,23 @@
-﻿[<AutoOpen>]
-module FsSql.AsyncExtensions
+﻿module FsSql.AsyncExtensions
 
+open System.Data
 open System.Data.SqlClient
 
 type SqlCommand with
     member cmd.AsyncExecNonQuery() =
-        async {
-            let abegin (a,b) = cmd.BeginExecuteNonQuery(a,b)
-            let aend = cmd.EndExecuteNonQuery
-            let acancel = cmd.Cancel
-            return! Async.FromBeginEnd(abegin, aend, acancel)
-        }
+        let abegin (a,b) = cmd.BeginExecuteNonQuery(a,b)
+        let aend = cmd.EndExecuteNonQuery
+        let acancel = cmd.Cancel
+        Async.FromBeginEnd(abegin, aend, acancel)
 
     member cmd.AsyncExecReader() =
-        async {
-            let abegin (a,b) = cmd.BeginExecuteReader(a,b)
-            let aend = cmd.EndExecuteReader
-            let acancel = cmd.Cancel
-            return! Async.FromBeginEnd(abegin, aend, acancel)
-        }
+        let abegin (a,b) = cmd.BeginExecuteReader(a,b)
+        let aend = cmd.EndExecuteReader
+        let acancel = cmd.Cancel
+        Async.FromBeginEnd(abegin, aend, acancel)
 
     member cmd.AsyncExecXmlReader() =
-        async {
-            let abegin (a,b) = cmd.BeginExecuteXmlReader(a,b)
-            let aend = cmd.EndExecuteXmlReader
-            let acancel = cmd.Cancel
-            return! Async.FromBeginEnd(abegin, aend, acancel)
-        }
+        let abegin (a,b) = cmd.BeginExecuteXmlReader(a,b)
+        let aend = cmd.EndExecuteXmlReader
+        let acancel = cmd.Cancel
+        Async.FromBeginEnd(abegin, aend, acancel)
