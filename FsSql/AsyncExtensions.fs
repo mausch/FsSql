e0929ffe000f03f8822c190643442aa21db05d57
mausch <mauricioscheffer@gmail.com>
2010-09-15 18:58:34 -0300

    inlined generic async functions

--- FsSql/AsyncExtensions.fs
+++ FsSql/AsyncExtensions.fs
@@ -1,7 +1,9 @@
 ﻿module FsSql.AsyncExtensions
 
+open System
 open System.Data
 open System.Data.SqlClient
+open System.Xml
 
 type SqlCommand with
     member cmd.AsyncExecNonQuery() =
@@ -21,3 +23,22 @@ type SqlCommand with
         let aend = cmd.EndExecuteXmlReader
         let acancel = cmd.Cancel
         Async.FromBeginEnd(abegin, aend, acancel)
+
+let inline asyncExecNonQuery cmd = 
+    let abegin (a,b:obj)= (^a: (member BeginExecuteNonQuery: AsyncCallback*obj -> IAsyncResult) (cmd,a,b))
+    let aend a = (^a: (member EndExecuteNonQuery: IAsyncResult -> int) (cmd,a))
+    let acancel () = (^a: (member Cancel: unit -> unit) (cmd))
+    Async.FromBeginEnd(abegin, aend, acancel)
+
+let inline asyncExecReader cmd =
+    let abegin (a,b:obj)= (^a: (member BeginExecuteReader: AsyncCallback*obj -> IAsyncResult) (cmd,a,b))
+    let aend a = (^a: (member EndExecuteReader: IAsyncResult -> #IDataReader) (cmd,a))
+    let acancel () = (^a: (member Cancel: unit -> unit) (cmd))
+    Async.FromBeginEnd(abegin, aend, acancel)
+
+let inline asyncExecXmlReader cmd =
+    let abegin (a,b:obj)= (^a: (member BeginExecuteXmlReader: AsyncCallback*obj -> IAsyncResult) (cmd,a,b))
+    let aend a = (^a: (member EndExecuteXmlReader: IAsyncResult -> XmlReader) (cmd,a))
+    let acancel () = (^a: (member Cancel: unit -> unit) (cmd))
+    Async.FromBeginEnd(abegin, aend, acancel)
+    
