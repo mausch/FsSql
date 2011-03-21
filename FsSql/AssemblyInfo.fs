313512f9509fd94d4a9b16e35b035ae6deb2c8d9
mausch <mauricioscheffer@gmail.com>
2011-03-21 12:01:08 -0300

    assembly info

---
+++ FsSql/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace FsSql
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("FsSql")>]
+[<assembly: AssemblyDescription("ADO.NET wrapper for F#")>]
+[<assembly: AssemblyProduct("FsSql")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2011")>]
+do()
