6f4374ad2f41d270aedc12797c3e50e912e97de9
mausch <mauricioscheffer@gmail.com>
2015-01-19 23:46:47 +0000

    Restore .NET-2.0 build

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
-@powershell -NoProfile -Command "(new-object net.webclient).DownloadFile('http://nuget.org/nuget.exe', 'NuGet.exe')"
+@powershell -NoProfile -Command "if (!(test-path 'NuGet.exe')) { (new-object net.webclient).DownloadFile('http://nuget.org/nuget.exe', 'NuGet.exe') }"
 NuGet.exe restore
-msbuild FsSql.sln
+msbuild /m /p:Configuration=Release /p:TargetFrameworkVersion=v2.0 /t:Rebuild FsSql.sln
+msbuild /m /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild FsSql.sln
+NuGet.exe pack
