dc951112b6d996306beb7a2a04efab53ff60495f
mausch <mauricioscheffer@gmail.com>
2015-02-17 01:57:13 +0000

    Bump version to 2.0

--- FsSql/AssemblyInfo.fs
+++ FsSql/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("2.0.0.0")>]
+[<assembly: AssemblyFileVersion("2.0.0.0")>]
 [<assembly: AssemblyTitle("FsSql")>]
 [<assembly: AssemblyDescription("ADO.NET wrapper for F#")>]
 [<assembly: AssemblyProduct("FsSql")>]
-[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2011-2014")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2011-2015")>]
 do()
