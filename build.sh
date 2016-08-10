52a62f7c5ec98370a210b23e8dfd3e9f49e9e1cf
Henrik Feldt <henrik@haf.se>
2016-08-10 13:40:20 +0200

    [infra] adding logging facade

--- build.sh
+++ build.sh
@@ -1,7 +1,38 @@
-NUGET=NuGet.exe
-if [ ! -f $NUGET ]; then
-  curl -L -o $NUGET https://www.nuget.org/nuget.exe
-  chmod a+x $NUGET
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+cd `dirname $0`
+OS=${OS:-"unknown"}
+
+function run() {
+  if [[ "$OS" != "Windows_NT" ]]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+[[ -f tools/paket.exe ]] || run tools/paket.bootstrapper.exe
+
+if [[ "$OS" != "Windows_NT" ]] && [ ! -e ~/.config/.mono/certs ]
+then
+  mozroots --import --sync --quiet
 fi
-mono $NUGET restore
-xbuild
+
+echo 'Restoring nugets'
+run tools/paket.exe restore
+
+echo 'Ensure Logging namespace'
+ruby -pi.bak -e \
+  "gsub(/namespace Logary.Facade/, 'namespace FsSql.Logging')" \
+  paket-files/logary/logary/src/Logary.Facade/Facade.fs
+
+
+echo 'Building'
+xbuild FsSql.sln /p:Configuration=Release
+
+echo 'Running tests'
+mono FsSql.Tests/bin/Release/FsSql.Tests.exe
